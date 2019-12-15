5677eb1c802f6d04985b01a692322ceb403ae608
Jane1016 <zjunwh@cn.ibm.com>
2019-12-16 01:38:50 +0800

    update

--- src/main/java/com/stocksba/company/apicontroller/ExchangeApi.java
+++ src/main/java/com/stocksba/company/apicontroller/ExchangeApi.java
@@ -9,10 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.stocksba.company.mapper.ExchangeMapper;
 import com.stocksba.company.model.Exchange;
+import com.stocksba.company.model.ExchangeCompany;
 import com.stocksba.company.rspmodel.RspModel;
 
 import io.swagger.annotations.Api;
@@ -54,6 +56,42 @@ public class ExchangeApi {
 				rsp.setCode(404);
 				rsp.setMessage("No Found Companys");
 				return new ResponseEntity<RspModel>(rsp, HttpStatus.OK);
+			}
+
+		} catch (Exception ex) {
+			RspModel rsp = new RspModel();
+			rsp.setCode(500);
+			rsp.setMessage(ex.getMessage());
+			return new ResponseEntity<RspModel>(rsp, HttpStatus.INTERNAL_SERVER_ERROR);
+
+		}
+	}
+	
+	@RequestMapping(value = "/query", method = RequestMethod.GET, produces = "application/json")
+	@ApiOperation(value = "SBA Company Name Query")
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "ok"), @ApiResponse(code = 400, message = "Bad Request"),
+			@ApiResponse(code = 401, message = "No Authroization"), @ApiResponse(code = 403, message = "No Permission"),
+			@ApiResponse(code = 404, message = "No Mentors Found"),
+			@ApiResponse(code = 500, message = "Internal Error") })
+	public ResponseEntity<RspModel> findCompanyCode(@ApiParam(name = "exchangeid", required = true) @RequestParam String exchangeid) {
+
+		try {
+
+			List<ExchangeCompany> companys = exchangemapper.getExchangeId(exchangeid);
+
+			if (companys.size() > 0 ) {
+
+				RspModel rsp = new RspModel();
+				rsp.setCode(200);
+				rsp.setMessage("Found Company");
+				rsp.setData(companys);
+				return new ResponseEntity<RspModel>(rsp, HttpStatus.OK);
+
+			} else {
+				RspModel rsp = new RspModel();
+				rsp.setCode(404);
+				rsp.setMessage("No Found Company");
+				return new ResponseEntity<RspModel>(rsp, HttpStatus.NOT_FOUND);
 			}
 
 		} catch (Exception ex) {
--- src/main/java/com/stocksba/company/mapper/ExchangeMapper.java
+++ src/main/java/com/stocksba/company/mapper/ExchangeMapper.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.SelectKey;
 import org.apache.ibatis.annotations.Update;
 
 import com.stocksba.company.model.Exchange;
+import com.stocksba.company.model.ExchangeCompany;
 
 @Mapper
 public interface ExchangeMapper {
@@ -22,5 +24,8 @@ public interface ExchangeMapper {
 	
 	@Update("update sba_company.exchange set name=#{name},brief=#{brief},address=#{address},remarks=#{remarks} where id = #{id}")
 	void updateExchange(Exchange exchange);
+	
+	@Select("SELECT id,name,code FROM sba_company.company where exchangeid =#{exchangeid}")
+	List<ExchangeCompany> getExchangeId(@Param("exchangeid") String exchangeid);
 
 }
